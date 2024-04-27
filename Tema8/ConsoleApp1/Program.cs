f812825550f290327d0d37d5a1be31b5137dd238
Artemid <Artemid@HOME-PC>
2024-04-27 22:49:11 +0300

    Выполнил Tema8

---
+++ Tema8/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+struct NOTE
+{
+    public string LastName;
+    public string FirstName;
+    public string PhoneNumber;
+    public int[] BirthDate;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        NOTE[] notes = new NOTE[8];
+
+        for (int i = 0; i < 8; i++)
+        {
+            Console.WriteLine("Введите фамилию:");
+            notes[i].LastName = Console.ReadLine();
+
+            Console.WriteLine("Введите имя:");
+            notes[i].FirstName = Console.ReadLine();
+
+            Console.WriteLine("Введите номер телефона:");
+            notes[i].PhoneNumber = Console.ReadLine();
+
+            Console.WriteLine("Введите дату рождения (день, месяц, год):");
+            notes[i].BirthDate = new int[3];
+            for (int j = 0; j < 3; j++)
+            {
+                notes[i].BirthDate[j] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        notes = notes.OrderBy(n => n.PhoneNumber.Substring(0, 3)).ToArray();
+
+        Console.WriteLine("Введите фамилию для поиска:");
+        string searchLastName = Console.ReadLine();
+
+        var note = notes.FirstOrDefault(n => n.LastName == searchLastName);
+
+        if (note.LastName != null)
+        {
+            Console.WriteLine($"Фамилия: {note.LastName}\nИмя: {note.FirstName}\nНомер телефона: {note.PhoneNumber}\nДата рождения: {string.Join(".", note.BirthDate)}");
+        }
+        else
+        {
+            Console.WriteLine("Человек с такой фамилией не найден.");
+        }
+    }
+}
---
+++ Tema8/ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public enum Position
+{
+    Director = 160, 
+    Manager = 180, 
+    Engineer = 200, 
+    Clerk = 220, 
+    Janitor = 240 
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Position director = Position.Director;
+        Position manager = Position.Manager;
+        Position engineer = Position.Engineer;
+        Position clerk = Position.Clerk;
+        Position janitor = Position.Janitor;
+
+        Console.WriteLine($"Директор должен отработать {director} часов в месяц.");
+        Console.WriteLine($"Менеджер должен отработать {manager} часов в месяц.");
+        Console.WriteLine($"Инженер должен отработать {engineer} часов в месяц.");
+        Console.WriteLine($"Клерк должен отработать {clerk} часов в месяц.");
+        Console.WriteLine($"Уборщик должен отработать {janitor} часов в месяц.");
+    }
+}
