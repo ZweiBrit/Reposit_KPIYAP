a4c6da9dbce85609b8715b0b925547109b9234c1
Artemid <Artemid@HOME-PC>
2024-04-29 12:38:12 +0300

    Добавил Tema9

---
+++ Tema9/ConsoleApp1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+interface Ix
+{
+    void IxF0(double parameter);
+    void IxF1();
+}
+
+interface Iy
+{
+    void F0(double parameter);
+    void F1();
+}
+
+interface Iz
+{
+    void F0(double parameter);
+    void F1();
+}
+
+class TestClass : Ix, Iy, Iz
+{
+    private double w;
+
+    void Iz.F0(double parameter)
+    {
+        Console.WriteLine($"Iz.F0: {Math.Sqrt(parameter)}");
+    }
+
+    void Iz.F1()
+    {
+        Console.WriteLine("Iz.F1");
+    }
+
+    public void IxF0(double parameter)
+    {
+        Console.WriteLine($"Ix.IxF0: {Math.Sqrt(parameter)}");
+    }
+
+    public void IxF1()
+    {
+        Console.WriteLine("Ix.IxF1");
+    }
+
+    public void F0(double parameter)
+    {
+        Console.WriteLine($"Iy.F0: {parameter * parameter + 5}");
+    }
+
+    public void F1()
+    {
+        Console.WriteLine("Iy.F1");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        TestClass obj = new TestClass();
+
+        obj.F0(4); 
+        ((Iz)obj).F0(4); 
+
+        ((Ix)obj).IxF0(16);
+        ((Ix)obj).IxF1();
+
+        Iy iyObj = obj;
+        iyObj.F1();
+    }
+}
---
+++ Tema9/ConsoleApp2/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+class FixedLengthStringArray
+{
+    private string[] array;
+    private int lowerBound;
+    private int upperBound;
+
+    public FixedLengthStringArray(int lowerBound, int upperBound)
+    {
+        if (lowerBound > upperBound)
+        {
+            throw new ArgumentException("Lower bound cannot be greater than upper bound.");
+        }
+
+        this.lowerBound = lowerBound;
+        this.upperBound = upperBound;
+        array = new string[upperBound - lowerBound + 1];
+    }
+
+    public string this[int index]
+    {
+        get
+        {
+            if (index < lowerBound || index > upperBound)
+            {
+                throw new IndexOutOfRangeException("Index is out of range.");
+            }
+
+            return array[index - lowerBound];
+        }
+        set
+        {
+            if (index < lowerBound || index > upperBound)
+            {
+                throw new IndexOutOfRangeException("Index is out of range.");
+            }
+
+            array[index - lowerBound] = value;
+        }
+    }
+
+    public FixedLengthStringArray Concatenate(FixedLengthStringArray other)
+    {
+        if (this.Length != other.Length)
+        {
+            throw new InvalidOperationException("Arrays must have the same length for concatenation.");
+        }
+
+        FixedLengthStringArray resultArray = new FixedLengthStringArray(lowerBound, upperBound);
+
+        for (int i = lowerBound; i <= upperBound; i++)
+        {
+            resultArray[i] = this[i] + other[i];
+        }
+
+        return resultArray;
+    }
+
+    public void PrintElement(int index)
+    {
+        Console.WriteLine($"Element at index {index}: {this[index]}");
+    }
+
+    public void PrintArray()
+    {
+        Console.WriteLine("Array:");
+        for (int i = lowerBound; i <= upperBound; i++)
+        {
+            Console.WriteLine($"[{i}]: {this[i]}");
+        }
+    }
+
+    public int Length => upperBound - lowerBound + 1;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        FixedLengthStringArray array1 = new FixedLengthStringArray(0, 4);
+        FixedLengthStringArray array2 = new FixedLengthStringArray(0, 4);
+
+        array1[0] = "Hello";
+        array1[1] = " ";
+        array1[2] = "world";
+        array1[3] = "!";
+        array1[4] = " ";
+
+        array2[0] = "How";
+        array2[1] = " ";
+        array2[2] = "are";
+        array2[3] = " ";
+        array2[4] = "you?";
+
+        array1.PrintArray();
+        array2.PrintArray();
+
+        FixedLengthStringArray concatenatedArray = array1.Concatenate(array2);
+        concatenatedArray.PrintArray();
+
+        array1.PrintElement(2);
+        array2.PrintElement(3);
+    }
+}
