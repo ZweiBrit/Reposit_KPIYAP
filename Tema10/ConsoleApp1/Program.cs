d18d074c2edf16b81121aaddd32449a304df7c0f
Artemid <Artemid@HOME-PC>
2024-04-30 13:14:47 +0300

    Добавил Tema10

---
+++ Tema10/ConsoleApp1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+class ParentClass
+{
+    private int parentField1;
+    private string parentField2;
+
+    public ParentClass(int field1, string field2)
+    {
+        parentField1 = field1;
+        parentField2 = field2;
+    }
+
+    public int GetParentField1()
+    {
+        return parentField1;
+    }
+
+    public string GetParentField2()
+    {
+        return parentField2;
+    }
+
+    public void DisplayParentFields()
+    {
+        Console.WriteLine("ParentField1: " + parentField1);
+        Console.WriteLine("ParentField2: " + parentField2);
+    }
+    public void ParentMethod()
+    {
+        Console.WriteLine("This is a method from the parent class.");
+    }
+}
+
+class ChildClass : ParentClass
+{
+    private double childField;
+
+    public ChildClass(int field1, string field2, double field3) : base(field1, field2)
+    {
+        childField = field3;
+    }
+
+
+    public void DisplayChildField()
+    {
+        Console.WriteLine("ChildField: " + childField);
+    }
+
+    public new void ParentMethod()
+    {
+        Console.WriteLine("This is a method from the child class.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ParentClass parentObj = new ParentClass(10, "ParentField");
+        ChildClass childObj = new ChildClass(20, "ParentField", 30.5);
+
+        Console.WriteLine("Base Class:");
+        parentObj.DisplayParentFields();
+        Console.WriteLine("ParentField1: " + parentObj.GetParentField1());
+        Console.WriteLine("ParentField2: " + parentObj.GetParentField2());
+        parentObj.ParentMethod();
+
+        Console.WriteLine();
+
+        Console.WriteLine("Derived Class:");
+        childObj.DisplayParentFields(); 
+        childObj.DisplayChildField(); 
+        Console.WriteLine("ParentField1: " + childObj.GetParentField1());
+        Console.WriteLine("ParentField2: " + childObj.GetParentField2());
+        childObj.ParentMethod();
+    }
+}
---
+++ Tema10/ConsoleApp2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class A
+{
+    protected int a = 5;
+    protected int b = 10;
+
+    public int C
+    {
+        get { return a + b; }
+    }
+}
+
+class B : A
+{
+    private int d = 20;
+
+    public B() { }
+
+    public B(int a, int b, int d)
+    {
+        this.a = a;
+        this.b = b;
+        this.d = d;
+    }
+
+
+    public int C2
+    {
+        get
+        {
+            if (a > b)
+                return a + b + d;
+            else
+                return a - b + d;
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        A objA = new A();
+
+        B objBDefault = new B();
+
+        B objBCustom = new B(7, 3, 25);
+
+        Console.WriteLine("Значение свойства C для объекта класса A: " + objA.C);
+        Console.WriteLine("Значение свойства C для объекта класса B (конструктор по умолчанию): " + objBDefault.C);
+        Console.WriteLine("Значение свойства C для объекта класса B (собственный конструктор): " + objBCustom.C);
+        Console.WriteLine("Значение свойства C2 для объекта класса B (конструктор по умолчанию): " + objBDefault.C2);
+        Console.WriteLine("Значение свойства C2 для объекта класса B (собственный конструктор): " + objBCustom.C2);
+    }
+}
