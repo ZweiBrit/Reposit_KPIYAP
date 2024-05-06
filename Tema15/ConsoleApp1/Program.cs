d07eed9716fd654e7cef00410c0b26119209ec47
Artemid <Artemid@HOME-PC>
2024-05-06 23:25:42 +0300

    Добавил Tema15

---
+++ Tema15/ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class MyList<T>
+{
+    private T[] items;
+    private int count;
+
+    public MyList()
+    {
+        items = new T[0];
+        count = 0;
+    }
+
+    public void Add(T item)
+    {
+        Array.Resize(ref items, count + 1);
+        items[count] = item;
+        count++;
+    }
+
+    public T this[int index]
+    {
+        get
+        {
+            if (index < 0 || index >= count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            return items[index];
+        }
+    }
+
+    public int Count
+    {
+        get { return count; }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyList<int> myList = new MyList<int>();
+
+        myList.Add(10);
+        myList.Add(20);
+        myList.Add(30);
+
+        Console.WriteLine("Elements in MyList:");
+        for (int i = 0; i < myList.Count; i++)
+        {
+            Console.WriteLine($"Element at index {i}: {myList[i]}");
+        }
+    }
+}
---
+++ Tema15/ConsoleApp2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyDictionary<TKey, TValue>
+{
+    private List<KeyValuePair<TKey, TValue>> items;
+
+    public MyDictionary()
+    {
+        items = new List<KeyValuePair<TKey, TValue>>();
+    }
+
+    public void Add(TKey key, TValue value)
+    {
+        items.Add(new KeyValuePair<TKey, TValue>(key, value));
+    }
+
+    public TValue this[TKey key]
+    {
+        get
+        {
+            foreach (var pair in items)
+            {
+                if (pair.Key.Equals(key))
+                {
+                    return pair.Value;
+                }
+            }
+            throw new KeyNotFoundException();
+        }
+    }
+
+    public int Count
+    {
+        get { return items.Count; }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyDictionary<string, int> myDictionary = new MyDictionary<string, int>();
+
+        myDictionary.Add("one", 1);
+        myDictionary.Add("two", 2);
+        myDictionary.Add("three", 3);
+
+        Console.WriteLine("Elements in MyDictionary:");
+        Console.WriteLine("Key: one, Value: " + myDictionary["one"]);
+        Console.WriteLine("Key: two, Value: " + myDictionary["two"]);
+        Console.WriteLine("Key: three, Value: " + myDictionary["three"]);
+    }
+}
