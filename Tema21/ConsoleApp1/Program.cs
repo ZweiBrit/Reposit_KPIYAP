31a137ee336cd56d271f0a1f2d90e0c663fc97c4
Artemid <Artemid@HOME-PC>
2024-05-14 23:37:20 +0300

    Добавил Tema21

---
+++ Tema21/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+
+        protected Singleton() { }
+
+        public static Singleton Instance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton singleton1 = Singleton.Instance();
+            Singleton singleton2 = Singleton.Instance();
+
+            if (singleton1 == singleton2)
+            {
+                Console.WriteLine("Оба экземпляра одинаковы");
+            }
+            else
+            {
+                Console.WriteLine("Экземпляры не одинаковы");
+            }
+        }
+    }
+}
